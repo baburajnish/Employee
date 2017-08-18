eccf86094214e9b418ebc0e99e6e15ef3e9e6e2c
baburajnish <baburajnish@gmail.com>
2017-08-19 03:41:26 +0530

    sql using

---
+++ Employee/WebForm1.aspx
@@ -0,0 +1,65 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Employee.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 25%;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td>
+                    <asp:DropDownList ID="DropDeptNameList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDeptNameList_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+                <td>
+                    <asp:Label ID="lblDname" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:DropDownList ID="DropEnoList" runat="server">
+                    </asp:DropDownList>
+                </td>
+                <td>
+                    <asp:Label ID="lblEno" runat="server"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td>Enter New Salary</td>
+                <td>
+                    <asp:TextBox ID="txtNewSal" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Updete" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblMsg" runat="server"></asp:Label>
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Employee/WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace Employee
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Server=RAJNISH-PC\SQLEXPRESS;Database=MyDatabase; Integrated Security=true ");
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Binddeptname();
+            }
+        }
+        internal void Binddeptname()
+        {
+            //conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from dept", conn);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds,"Deptnew");
+            DropDeptNameList.DataSource = ds.Tables["Deptnew"];
+            DropDeptNameList.DataTextField = "deptName";
+            DropDeptNameList.DataValueField = "deptNo";
+            DropDeptNameList.DataBind();
+            DropDeptNameList.Items.Insert(0,"Select");
+        }
+
+        
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            lblMsg.Text = "";
+            lblEno.Text = "";
+            if (DropEnoList.SelectedIndex == 0)
+            {
+                lblEno.Text = "Pls Select Valid EmpNo..";
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("Update emp set salary=@newsalary where empno=@eno",conn);
+                cmd.Parameters.AddWithValue("@eno",DropEnoList.SelectedValue);
+                cmd.Parameters.AddWithValue("@newsalary",txtNewSal.Text);
+                conn.Open();
+                int i = cmd.ExecuteNonQuery();
+                conn.Close();
+                if (i == 1)
+                {
+                    lblMsg.Text = "Salary is updated";
+                }
+                else
+                {
+                    lblMsg.Text = "Salary is not updated";
+                }
+                DropDeptNameList.SelectedIndex = 0;
+                DropEnoList.SelectedIndex = 0;
+                txtNewSal.Text = "";
+                txtNewSal.Focus();
+            }
+        }
+
+        protected void DropDeptNameList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblDname.Text = "";
+            lblEno.Text = "";
+            lblMsg.Text = "";
+            txtNewSal.Text = "";
+            DropEnoList.Items.Clear();
+            if (DropDeptNameList.SelectedIndex == 0)
+            {
+                lblDname.Text = "*Pls Select Valid Name..";
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("Select empno from emp where deptno = @empno", conn);
+
+                cmd.Parameters.AddWithValue("@empno", DropDeptNameList.SelectedValue);
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "empnew");
+                DropEnoList.DataSource = ds.Tables["empnew"];
+                DropEnoList.DataTextField = "empno";
+                DropEnoList.DataBind();
+                DropEnoList.Items.Insert(0, "Select");
+
+            }
+        }
+    }
+}
